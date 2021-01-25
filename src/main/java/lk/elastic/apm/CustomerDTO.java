1c03d7d618942b01b74e808b28c2cc4c9d9d536f
stousmand <shirhan.apache@gmail.com>
2021-01-25 15:52:41 +0530

    Adds CustomerDTO and refactors REST endpoint
    Adds CustomerDTO and refactors REST endpoint
    
    CustomerDTO was added as a test DTO  class. The REST endpoint method was refatored so that the DTO object is obtained through wiring a Spring Service component.

---
+++ src/main/java/lk/elastic/apm/CustomerDTO.java
@@ -0,0 +1,28 @@
+package lk.elastic.apm;
+
+public class CustomerDTO {
+	private int id;
+	private String customerName;
+	private int customerAge;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public int getCustomerAge() {
+		return customerAge;
+	}
+	public void setCustomerAge(int customerAge) {
+		this.customerAge = customerAge;
+	}
+	
+	
+}
---
+++ src/main/java/lk/elastic/apm/CustomerServiceResource.java
@@ -0,0 +1,22 @@
+package lk.elastic.apm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.elastic.apm.api.ElasticApm;
+import co.elastic.apm.api.Span;
+
+@RestController
+public class CustomerServiceResource {
+	
+	@Autowired
+	private CustomerService cusService;
+	
+	@GetMapping("/customer")
+	public CustomerDTO getCustomer(@RequestHeader HttpHeaders headers) {
+		return cusService.getCustomer();
+	}
+}
